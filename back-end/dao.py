c903ade2620412f1a04a86b0de3a2e2fa76634f2
Marcelo Scarton <scartonmarcelo@gmail.com>
2020-09-24 20:50:56 -0300

    Ajustes na validação do perfil

--- back-end/dao.py
+++ back-end/dao.py
@@ -4,7 +4,7 @@ from pymongo import errors, MongoClient
 
 db_pass = os.environ.get('DBPASS')
 client = MongoClient(
-    f"mongodb+srv://yanhkawakami:jurandir123@pisi2020.qelym.mongodb.net/")
+    f"mongodb+srv://yanhkawakami:{db_pass}@pisi2020.qelym.mongodb.net/")
 
 
 class UserDao:
@@ -26,6 +26,11 @@ class UserDao:
         try:
             if (password != valida_password):
                 self.message = "As senhas não coincidem"
+                return False
+            query = {"user": user}
+            doc = self.col.find_one(query)
+            if (doc != None):
+                self.message = "Nome de usuário em uso"
                 return False
             doc = {"_id": email, "user": user, "pass": password}
             self.col.insert_one(doc)
