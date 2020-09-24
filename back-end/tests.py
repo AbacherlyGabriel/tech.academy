d6d3d18815dfc743dc0a0e03e755be5f7b0758c6
Marcelo Scarton <scartonmarcelo@gmail.com>
2020-09-24 20:59:27 -0300

    Ajuste no método de testar criação de usuários

--- back-end/tests.py
+++ back-end/tests.py
@@ -6,4 +6,4 @@ def test_user_login():
     assert user_dao.read("admin@admin.com", "pass") == True
 
 def test_user_creation():
-    assert user_dao.create("admin@admin.com", "admin", "pass") == False
+    assert user_dao.create("admin@admin.com", "admin", "pass", "pass") == False
