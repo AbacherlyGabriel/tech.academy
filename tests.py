b9162faa926dc477b04a73dc2ca00558e0059407
Marcelo Scarton <scartonmarcelo@gmail.com>
2020-09-25 01:32:32 -0300

    Adição de teste e estilização de código

--- tests.py
+++ tests.py
@@ -1,4 +1,9 @@
 from src.dao import UserDao
 
+
 def test_dao_read():
-    assert UserDao().read("admin@admin.com", "pass") == True
+    assert UserDao().read("admin@admin.com", "pass") is True
+
+
+def test_dao_create():
+    assert UserDao().create("admin@admin.com", "admin", "pass", "pass") is False
