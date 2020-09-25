e6a17f28c829413b606773a192009bea00f4d761
Marcelo Scarton <scartonmarcelo@gmail.com>
2020-09-24 21:35:04 -0300

    Readequação da estrutura

--- front-end-tests/js/forms.js
+++
@@ -1,5 +0,0 @@
-window.onload = function(){
-    
-    document.getElementById("login-form").reset();
-
-}
---
+++ tests.py
@@ -0,0 +1,9 @@
+from src.dao import UserDao
+
+user_dao = UserDao()
+
+def test_user_login():
+    assert user_dao.read("admin@admin.com", "pass") == True
+
+def test_user_creation():
+    assert user_dao.create("admin@admin.com", "admin", "pass", "pass") == False
