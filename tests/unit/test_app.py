8e1a814fcb3c0948e8b8bc0a1a96a3e4cee8f0bc
Rodrigo Seiji Campos Imai <rseiji@uolinc.com>
2020-11-05 01:08:44 -0300

    Adiciona validações nos métodos dos testes

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -5,30 +5,35 @@ def test_home_page():
     with app.test_client() as test_client:
         response = test_client.get('/')
         assert response.status_code == 200
+        assert b"tech.academy | home" in response.data
 
 
 def test_register_page():
     with app.test_client() as test_client:
         response = test_client.get('/register')
         assert response.status_code == 200
+        assert b"tech.academy | sign up" in response.data
 
 
 def test_login_page():
     with app.test_client() as test_client:
         response = test_client.get('/login')
         assert response.status_code == 200
+        assert b"tech.academy | login" in response.data
 
 
 def test_about_page():
     with app.test_client() as test_client:
         response = test_client.get('/about')
         assert response.status_code == 200
+        assert b"tech.academy | about" in response.data
 
 
 def test_profile_page():
     with app.test_client() as test_client:
         response = test_client.get('/perfil')
         assert response.status_code == 200
+        assert b"ech.academy | meu perfil" in response.data
 
 
 def test_page_not_found():
